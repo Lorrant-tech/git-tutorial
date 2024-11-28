beb733d89a060f00ce4a6334b59bc68a2d2f1805
Lorrant <lorrant@teste.com>
2024-11-28 16:26:19 -0300

    Version 14 shh

--- changes.js
+++ changes.js
@@ -1,2 +1,3 @@
 console.log("Changes V1");
 console.log("version Github 13");
+console.log("version SSH");
--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("Main V1");
 console.log("version Github 13");
+console.log("version SSH");
